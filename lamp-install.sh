4ee2ad7967ca4c42d837c2ffeade9c2a756017a2
supergnaw <loren.supernaw@gmail.com>
2018-08-12 09:03:05 +0900

    Update lamp-install.sh

--- lamp-install.sh
+++ lamp-install.sh
@@ -22,6 +22,73 @@ systemctl start mariadb
 # 1.3 php
 echo "Installing PHP, MySQL, and phpMyAdmin..."
 yum -y install php php-mysql phpmyadmin
+# special shoutout to this tutorial for making this a reality:
+#	http://www.configserverfirewall.com/linux-tutorials/install-phpmyadmin-centos-7/
+# for phpma verions go to
+#	https://www.phpmyadmin.net/downloads/
+curl -O https://files.phpmyadmin.net/phpMyAdmin/4.8.2/phpMyAdmin-4.8.2-all-languages.tar.gz
+tar -zxvf phpMyAdmin-4.8.2-all-languages.tar.gz --directory /usr/share
+mv phpMyAdmin-4.8.2-all-languages phpmyadmin
+echo 'Alias /phpMyAdmin /usr/share/phpmyadmin
+Alias /phpmyadmin /usr/share/phpmyadmin
+
+<Directory /usr/share/phpmyadmin/>
+    AddDefaultCharset UTF-8
+
+    <IfModule mod_authz_core.c>
+      # Apache 2.4
+      <RequireAny>
+        Require all granted
+        Require ip 127.0.0.1
+        Require ip ::1
+      </RequireAny>
+    </IfModule>
+    <IfModule !mod_authz_core.c>
+      # Apache 2.2
+      Order Deny,Allow
+      Deny from All
+      Allow from 127.0.0.1
+      Allow from ::1
+    </IfModule>
+</Directory>
+
+<Directory /usr/share/phpmyadmin/setup/>
+    <IfModule mod_authz_core.c>
+      # Apache 2.4
+      <RequireAny>
+        Require ip 127.0.0.1
+        Require ip ::1
+      </RequireAny>
+    </IfModule>
+    <IfModule !mod_authz_core.c>
+      # Apache 2.2
+      Order Deny,Allow
+      Deny from All
+      Allow from 127.0.0.1
+      Allow from ::1
+    </IfModule>
+</Directory>
+
+# These directories do not require access over HTTP - taken from the original
+# phpmyadmin upstream tarball
+#
+<Directory /usr/share/phpmyadmin/libraries/>
+    Order Deny,Allow
+    Deny from All
+    Allow from None
+</Directory>
+
+<Directory /usr/share/phpmyadmin/setup/lib/>
+    Order Deny,Allow
+    Deny from All
+    Allow from None
+</Directory>
+
+<Directory /usr/share/phpmyadmin/setup/frames/>
+    Order Deny,Allow
+    Deny from All
+    Allow from None
+</Directory>' > /etc/httpd/conf.d/phpmyadmin.conf
 
 # 1.4 test/restart service
 echo "Testing config..."
